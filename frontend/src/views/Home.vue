00cb868930fa549205e7f99642e3f564bd374477
b21827282 <basakcontu@hacettepe.edu.tr>
2021-04-14 01:43:12 +0300

    frontend with sign in

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <button class="login" @click="goToSignIn()">Log In</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: "Home",
+  methods: {
+    goToSignIn(){
+      this.$router.push({path: "/SignIn"});
+    }
+  },
+  components: {
+    
+  },
+};
+</script>
