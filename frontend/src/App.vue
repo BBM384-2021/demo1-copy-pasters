9bd568fc50dbfcf12b7ceffda59497a5d39ceba6
b21827282 <basakcontu@hacettepe.edu.tr>
2021-04-18 21:09:06 +0300

    App.vue css fixed, Homepage desgined, background picture added

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -16,19 +16,23 @@
   color: #2c3e50;
   display: flex;
   justify-content: center;
-  align-items: center;*/
-  font-family: Tahoma;
-    font-size: 1.6rem;
-    color: rgb(156, 89, 156);
-    background-color: #f8f8f8;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  align-items: center; */
+ /* font-family: Tahoma;
+  font-size: 1.6rem;
+  background-color: #f8f8f8;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center; */
+    
 }
-
+body,html{
+  height: 100%;
+  margin: 0;
+ 
+}
 #nav {
-  padding: 30px;
+ /* padding: 30px;
 
   a {
     font-weight: bold;
@@ -37,6 +41,8 @@
     &.router-link-exact-active {
       color: #42b983;
     }
-  }
+  }  */
+ 
 }
+
 </style>
