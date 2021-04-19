105c35d0e386e2da250c7611bf11ba5880fe5fd3
b21827282 <44412189+b21827282@users.noreply.github.com>
2021-04-19 18:27:07 +0300

    Update index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,9 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import SignIn from "../components/SignIn.vue"
+import SignUp from "../components/SignUp.vue"
+import Questionnaire from "../components/Questionnaire.vue";
+import QuestionnaireResult from "../components/QuestionnaireResult.vue"
 Vue.use(VueRouter);
 
 const routes = [
@@ -16,8 +19,27 @@ const routes = [
     name: SignIn,
     component:() =>
       import("../components/SignIn.vue")
-  }
+  },
+  {
+    path: "/SignUp",
+    name: SignUp,
+    component:() =>
+      import("../components/SignUp.vue")
+  },
 
+  {
+    path:"/questionnaire",
+    name:Questionnaire,
+    component: () =>
+      import( "../components/Questionnaire.vue")
+  },
+  {
+    path:"/questionnaire-result-page",
+    name:QuestionnaireResult,
+    component: () =>
+      import( "../components/QuestionnaireResult.vue")
+  }
+  
 ];
 
 const router = new VueRouter({
